ba34c8fbb82952c56acf0727699e550885faca7f
Boyd Harghaz <bharghaz@at-i7.msk.21-school.ru>
2022-08-16 18:50:45 +0300

    cgi post args

--- res/cgi-bin/test.py
+++ res/cgi-bin/test.py
@@ -1,1 +1,6 @@
-print("<html><head></head><body><h2>Hello world from Python</h2></body></html>")
+from future import print_function
+import os
+
+print("<html><head></head><body><h2>Hello,", end=' ')
+print(os.environ['name'], end=' ')
+print("from Python</h2></body></html>")
